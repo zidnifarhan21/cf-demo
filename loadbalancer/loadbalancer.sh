9477ba695165a8c6b5c71fa6ac3950c5ae498242
zidnifarhan21 <60363735+zidnifarhan21@users.noreply.github.com>
2024-03-08 07:57:01 +0700

    upload

---
+++ loadbalancer/loadbalancer.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+CFN_FILE="loadbalancer.yaml"
+#BUCKET="" 
+REGION="us-east-1" 
+STACK_NAME="WebAppALB"
+PARAMETERS=$(jq -r '.[] | "\(.ParameterKey)=\(.ParameterValue)"' parameters.json | tr '\n' ' ')
+
+# deploy
+aws cloudformation deploy \
+  --stack-name $STACK_NAME \
+  --region $REGION \
+  --template-file "$CFN_FILE" \
+  --tags group=WebApp-ALB\
+  --disable-rollback \
+  --parameter-overrides $PARAMETERS \
+  --capabilities CAPABILITY_NAMED_IAM
+
+
+# use if review process needed
+#   --no-execute-changeset \
---
+++ network/network.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CFN_FILE="network.yaml"
+#BUCKET=""
+REGION="us-east-1" 
+STACK_NAME="Network"
+PARAMETERS=$(jq -r '.[] | "\(.ParameterKey)=\(.ParameterValue)"' parameters.json | tr '\n' ' ')
+
+# deploy
+aws cloudformation deploy --region $REGION --stack-name $STACK_NAME --template-file "$CFN_FILE" --no-execute-changeset --tags group=WepApp-compute --parameter-overrides $PARAMETERS --capabilities CAPABILITY_NAMED_IAM
