a527da82648ff4c83e6353c633a276fa805076cd
zidnifarhan21 <60363735+zidnifarhan21@users.noreply.github.com>
2024-03-12 23:37:06 +0700

    edit loadbalancer stack

--- loadbalancer/loadbalancer.sh
+++ loadbalancer/loadbalancer.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 CFN_FILE="loadbalancer.yaml"
 #BUCKET="" 
@@ -7,14 +7,11 @@ STACK_NAME="WebAppALB"
 PARAMETERS=$(jq -r '.[] | "\(.ParameterKey)=\(.ParameterValue)"' parameters.json | tr '\n' ' ')
 
 # deploy
-aws cloudformation deploy \
-  --stack-name $STACK_NAME \
-  --region $REGION \
-  --template-file "$CFN_FILE" \
-  --tags group=WebApp-ALB\
-  --disable-rollback \
-  --parameter-overrides $PARAMETERS \
-  --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --stack-name $STACK_NAME \
+ --region $REGION --template-file "$CFN_FILE" \
+ --tags group=WebApp-ALB --disable-rollback \
+ --parameter-overrides $PARAMETERS \
+ --capabilities CAPABILITY_NAMED_IAM
 
 
 # use if review process needed
